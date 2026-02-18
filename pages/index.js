ab4d8fefced940dbd6d1e8ef38e739f8aafaf997
Gabriel Neves <gabrielbatellineves@gmail.com>
2026-02-18 01:05:23 +0000

    Alterado mensagem da pagina inicial.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Tom Tom você prefere ser chamada de Amelha ou Belho?</h1>
+    return <h1>Tom Tom você prefere ser chamada de Amelha ou Belho? 🐝 <br/>Te amo mais do que tudo meu amor!</h1>
+
 }
 
 export default Home;
